594a13a77d9fcf991fef64afa25da13dadfeb49d
crduque <crduque@hotmail.com>
2020-10-14 15:27:23 +0000

    adding new files

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,21 @@ import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
-};
+var listenerFunction = function () {
+    document.querySelectorAll("onchange");
+}
+function isText() {
+    var string = document.querySelector("#name").value;
+    if (typeof string == "string"){
+        return true;
+    } else{ 
+        return false;
+    }
+}
+// function validateForm() {
+//   var x = document.forms["myForm"]["fname"].value;
+//   if (x == "") {
+//     alert("Name must be filled out");
+//     return false;
+//   }
+// }
